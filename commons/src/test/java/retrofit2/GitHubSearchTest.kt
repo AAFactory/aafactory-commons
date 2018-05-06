3bc29c9f55925a0e2805d4d4889955f9a65c60e7
hanjoong.cho <hanjoongcho@gmail.com>
2018-05-06 13:02:02 +0900

    Added retrofit2 library test case

--- commons/src/test/java/retrofit2/GitHubSearchTest.kt
+++ commons/src/test/java/retrofit2/GitHubSearchTest.kt
@@ -1,5 +1,8 @@
 package retrofit2
 
+import okhttp3.ResponseBody
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 import org.junit.Assert
 import org.junit.Test
 import retrofit2.converter.gson.GsonConverterFactory
@@ -7,6 +10,9 @@ import retrofit2.helpers.ToStringConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.io.IOException
+import java.lang.reflect.Type
+import java.util.*
 
 /**
  * Created by CHO HANJOONG on 2018-05-06.
@@ -23,7 +29,7 @@ class GitHubSearchTest() {
 
         val retrofit = Retrofit.Builder()
                 .baseUrl(API_URL)
-                .addConverterFactory(ToStringConverterFactory())
+                .addConverterFactory(RepositoryAdapter.createRepositoryAdapter)
                 .build()
 
         // Create an instance of our GitHub API interface.
@@ -42,6 +48,37 @@ class GitHubSearchTest() {
         fun contributors(
                 @Query("q") question: String,
                 @Query("utf8") utf8: String
-        ): Call<String>
+        ): Call<Repository>
+    }
+
+    data class Repository(val name: String, val description: String, val link: String)
+
+    internal class RepositoryAdapter : Converter<ResponseBody, Repository> {
+
+        @Throws(IOException::class)
+        override fun convert(responseBody: ResponseBody): Repository {
+            val document = Jsoup.parse(responseBody.string())
+            val repoList = document.select(".repo-list")
+            if (repoList.isNotEmpty()) {
+                val list: Elements? = repoList.select("div")
+                list?.let { 
+                    println(it)
+                }
+            }
+            
+            return Repository("", "", "")
+        }
+
+        companion object {
+            val createRepositoryAdapter: Converter.Factory = object : Converter.Factory() {
+                override fun responseBodyConverter(
+                        type: Type,
+                        annotations: Array<Annotation>,
+                        retrofit: Retrofit
+                ): Converter<ResponseBody, *>? {
+                    return if (type === Repository::class.java) RepositoryAdapter() else null
+                }
+            }
+        }
     }
 }
