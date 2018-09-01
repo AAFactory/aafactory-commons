3d6c01842118a1bb05f01c26417e0b6c4ef3ab13
hanjoong.cho <hanjoongcho@gmail.com>
2018-09-01 16:26:51 +0900

    Update sample code to  'material-dialogs' module

--- app/src/main/java/com/afollestad/materialdialogssample/MainActivity.kt
+++ app/src/main/java/com/afollestad/materialdialogssample/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.afollestad.materialdialogssample
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import com.afollestad.materialdialogs.MaterialDialog
 import io.github.aafactory.sample.R
 import io.github.aafactory.sample.helpers.SampleActivity
-import kotlinx.android.synthetic.main.bubbleseekbar_activity_main.*
+import kotlinx.android.synthetic.main.materialdialog_activity_main.*
 
 /**
  * Created by CHO HANJOONG on 2018-09-01.
@@ -18,5 +21,48 @@ class MainActivity : SampleActivity() {
         supportActionBar?.run {
             setDisplayHomeAsUpEnabled(true)
         }
+
+        basic_titled_buttons.setOnClickListener {
+            MaterialDialog(this).show {
+                title(R.string.useGoogleLocationServices)
+                message(R.string.useGoogleLocationServicesPrompt)
+                positiveButton(R.string.agree)
+                negativeButton(R.string.disagree)
+            }
+        }
+
+        basic_icon.setOnClickListener {
+            MaterialDialog(this).show {
+                title(R.string.useGoogleLocationServices)
+                icon(R.mipmap.ic_launcher)
+                message(R.string.useGoogleLocationServicesPrompt)
+                positiveButton(R.string.agree)
+                negativeButton(R.string.disagree)
+            }
+        }
+
+        buttons_callbacks.setOnClickListener {
+            MaterialDialog(this).show {
+                title(R.string.useGoogleLocationServices)
+                message(R.string.useGoogleLocationServicesPrompt)
+                positiveButton(R.string.agree) { _ ->
+                    toast("On positive")
+                }
+                negativeButton(R.string.disagree) { _ ->
+                    toast("On negative")
+                }
+                neutralButton(R.string.more_info) { _ ->
+                    toast("On neutral")
+                }
+            }
+        }
+    }
+
+
+    var toast: Toast? = null
+    internal fun Activity.toast(message: CharSequence) {
+        toast?.cancel()
+        toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast!!.show()
     }
 }
