77c2ea278eb06c45eacc7b649f1570dd8191154e
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2019-01-03 06:55:31 +0900

    Remove 'com.luffykou:android-common-utils:1.1.3' dependency

--- commons/src/main/java/io/github/aafactory/commons/utils/LocationUtils.kt
+++ commons/src/main/java/io/github/aafactory/commons/utils/LocationUtils.kt
@@ -3,9 +3,11 @@ package io.github.aafactory.commons.utils
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.location.Location
+import android.support.design.widget.Snackbar
+import android.view.View
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.tasks.Task
-import com.litesuits.common.utils.DialogUtil
+import io.github.aafactory.commons.extensions.showSnackBar
 
 /**
  * Created by CHO HANJOONG on 2018-05-29.
@@ -20,8 +22,9 @@ class LocationUtils {
                         if (task.isSuccessful && task.result != null) {
                             callBack(task)
                         } else {
-                            DialogUtil.showTips(activity, "", task.exception.toString())
+//                            DialogUtil.showTips(activity, "", task.exception.toString())
 //                        showSnackbar(R.string.no_location_detected)
+                            activity.findViewById<View>(android.R.id.content).showSnackBar(task.exception.toString(), Snackbar.LENGTH_LONG)
                         }
                     }
         }
