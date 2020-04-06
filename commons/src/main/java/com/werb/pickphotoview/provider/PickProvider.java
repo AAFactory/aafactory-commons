7b4613e7dd00171e0401709bedfcdf274979b692
hanjoongcho\hanjoong <hanjoongcho@gmail.com>
2020-04-07 06:18:58 +0900

    Directly imports the pickphotoview library to solve the scoped storage issue

---
+++ commons/src/main/java/com/werb/pickphotoview/provider/PickProvider.java
@@ -0,0 +1,10 @@
+package com.werb.pickphotoview.provider;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by wanbo on 2017/1/16.
+ */
+
+public class PickProvider extends FileProvider {
+}
