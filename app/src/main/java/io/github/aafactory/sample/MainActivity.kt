9f8c805de5fb527fa56a1214bb3013dac3447647
DESKTOP-2T3RPD2\hanjoong.cho <hanjoongcho@gmail.com>
2021-10-04 00:44:26 +0900

    Refactoring

--- app/src/main/java/io/github/aafactory/sample/MainActivity.kt
+++ app/src/main/java/io/github/aafactory/sample/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import io.github.aafactory.commons.activities.BaseSimpleActivity
 import io.github.aafactory.commons.activities.MarkDownViewActivity
@@ -17,84 +16,34 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : BaseSimpleActivity() {
-    val listItems = mutableListOf<Map<String, String>>(
-            mapOf("owner" to "Werb",
-                    "name" to "PickPhotoSample")
-            , mapOf("owner" to "ParkSangGwon",
-                    "name" to "TedBottomPicker")
-            , mapOf("owner" to "donglua",
-                    "name" to "PhotoPicker")
-            , mapOf("owner" to "gsuitedevs",
-                    "name" to "android-samples",
-                    "displayName" to "Google Drive Android API Demos")
-            , mapOf("owner" to "kioko",
-                    "name" to "motion-layout-playground")
-            , mapOf("owner" to "zoonooz",
-                    "name" to "simple-view-behavior")
-            , mapOf("owner" to "googlesamples",
-                    "name" to "android-architecture",
-                    "displayName" to "todo-mvvm-live-kotlin"
-            )
-            , mapOf("owner" to "googlesamples",
-                "name" to "android-architecture",
-                "displayName" to "todo-mvp-kotlin"
-            )
-            , mapOf("owner" to "afollestad",
-                    "name" to "material-dialogs"
-            )
-            , mapOf("owner" to "googlesamples",
-                    "name" to "android-FingerprintDialog"
-            )
-            , mapOf("owner" to "rubensousa",
-                    "name" to "ViewPagerCards"
-            )
-            , mapOf("owner" to "AAFactory",
-                    "name" to "aafactory-commons"
-            )
-            , mapOf("owner" to "devunwired",
-                    "name" to "recyclerview-playground"
-            )
-            , mapOf("owner" to "openlayers",
-                    "name" to "openlayers"
-            )
-            , mapOf("owner" to "juanchosaravia",
-                    "name" to "KedditBySteps"
-            )
-            , mapOf("owner" to "Tapadoo",
-                    "name" to "Alerter"
-            )
-            , mapOf("owner" to "saulmm",
-                    "name" to "CoordinatorExamples"
-            )
-            , mapOf("owner" to "medyo",
-                    "name" to "Fancybuttons"
-            )
-            , mapOf("owner" to "pedant",
-                    "name" to "sweet-alert-dialog"
-            )
-            , mapOf("owner" to "timusus",
-                    "name" to "RecyclerView-FastScroll"
-            )
-            , mapOf("owner" to "woxingxiao",
-                    "name" to "BubbleSeekBar"
-            )
-            , mapOf("owner" to "PhilJay",
-                    "name" to "MPAndroidChart"
-            )
-            , mapOf("owner" to "navermaps",
-                    "name" to "maps.android"
-            )
-            , mapOf("owner" to "bumptech",
-                    "name" to "glide",
-                    "displayName" to "glide-imgur"
-            )
-            , mapOf("owner" to "bumptech",
-                    "name" to "glide",
-                    "displayName" to "glide-flickr"
-            )
-            , mapOf("owner" to "googlesamples",
-            "name" to "android-ConstraintLayoutExamples"
-    )
+    val listItems = mutableListOf(
+            mapOf("owner" to "AppIntro", "name" to "AppIntro")
+            , mapOf("owner" to "Werb", "name" to "PickPhotoSample")
+            , mapOf("owner" to "ParkSangGwon", "name" to "TedBottomPicker")
+            , mapOf("owner" to "donglua", "name" to "PhotoPicker")
+            , mapOf("owner" to "gsuitedevs", "name" to "android-samples", "displayName" to "Google Drive Android API Demos")
+            , mapOf("owner" to "kioko", "name" to "motion-layout-playground")
+            , mapOf("owner" to "zoonooz", "name" to "simple-view-behavior")
+            , mapOf("owner" to "googlesamples", "name" to "android-architecture", "displayName" to "todo-mvvm-live-kotlin")
+            , mapOf("owner" to "googlesamples", "name" to "android-architecture", "displayName" to "todo-mvp-kotlin")
+            , mapOf("owner" to "afollestad", "name" to "material-dialogs")
+            , mapOf("owner" to "googlesamples", "name" to "android-FingerprintDialog")
+            , mapOf("owner" to "rubensousa", "name" to "ViewPagerCards")
+            , mapOf("owner" to "AAFactory", "name" to "aafactory-commons")
+            , mapOf("owner" to "devunwired", "name" to "recyclerview-playground")
+            , mapOf("owner" to "openlayers", "name" to "openlayers")
+            , mapOf("owner" to "juanchosaravia", "name" to "KedditBySteps")
+            , mapOf("owner" to "Tapadoo", "name" to "Alerter")
+            , mapOf("owner" to "saulmm", "name" to "CoordinatorExamples")
+            , mapOf("owner" to "medyo", "name" to "Fancybuttons")
+            , mapOf("owner" to "pedant", "name" to "sweet-alert-dialog")
+            , mapOf("owner" to "timusus", "name" to "RecyclerView-FastScroll")
+            , mapOf("owner" to "woxingxiao", "name" to "BubbleSeekBar")
+            , mapOf("owner" to "PhilJay", "name" to "MPAndroidChart")
+            , mapOf("owner" to "navermaps", "name" to "maps.android")
+            , mapOf("owner" to "bumptech", "name" to "glide", "displayName" to "glide-imgur")
+            , mapOf("owner" to "bumptech", "name" to "glide", "displayName" to "glide-flickr")
+            , mapOf("owner" to "googlesamples", "name" to "android-ConstraintLayoutExamples")
     )
     private var mListItem: ArrayList<Showcase> = arrayListOf<Showcase>()
     private val adapter: ShowcaseAdapter by lazy { 
