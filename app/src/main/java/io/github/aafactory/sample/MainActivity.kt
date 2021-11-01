30fbeb33f23df82ffd175db8b99afa9ca9a47eeb
DESKTOP-2T3RPD2\hanjoong.cho <hanjoongcho@gmail.com>
2021-11-02 00:42:06 +0900

    Refactoring

--- app/src/main/java/io/github/aafactory/sample/MainActivity.kt
+++ app/src/main/java/io/github/aafactory/sample/MainActivity.kt
@@ -5,12 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.simplemobiletools.commons.extensions.showKeyboard
@@ -31,7 +28,7 @@ class MainActivity : BaseSimpleActivity() {
     private lateinit var mDialogSearchMainBinding: DialogSearchMainBinding
 
     private val showcaseItems = mutableListOf(
-            Showcase("", "Regular Expression", true, "Basic Chapter-01", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/regular-expression/chapter01.md"),
+            Showcase("hanjoongcho", "CheatSheet", false, ""),
             Showcase("AppIntro", "AppIntro", false, ""),
             Showcase("Werb", "PickPhotoSample", false, ""),
             Showcase("ParkSangGwon", "TedBottomPicker", false, ""),
@@ -57,7 +54,35 @@ class MainActivity : BaseSimpleActivity() {
             Showcase("navermaps", "maps.android", false, ""),
             Showcase("bumptech", "glide", false, ""),
             Showcase("googlesamples", "android-ConstraintLayoutExamples", false, ""),
-    )
+            Showcase("", "Regular Expression", true, "Basic Chapter-01", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/regular-expression/chapter01.md"),
+            Showcase("", "Git-01", true, "Git and Git Flow Cheat Sheet", "https://raw.githubusercontent.com/arslanbilal/git-cheat-sheet/master/other-sheets/git-cheat-sheet-ko.md"),
+            Showcase("", "Android-01", true, "Project guidelines", "https://raw.githubusercontent.com/hanjoongcho/android-guidelines/master/project_and_code_guidelines.md"),
+            Showcase("", "Android-02", true, "Architecture Guidelines / MVP", "https://raw.githubusercontent.com/hanjoongcho/android-guidelines/master/architecture_guidelines/android_architecture.md"),
+            Showcase("", "Kotlin-01", true, "Explanation of kotlin basic functions", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/kotlin/kotlin.md"),
+            Showcase("", "Kotlin-02", true, "Explanation of kotlin collection functions", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/kotlin/kotlin.collections.md"),
+            Showcase("", "Kotlin-03", true, "Coding Conventions", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/kotlin/coding-conventions.md"),
+            Showcase("", "ES6-01", true, "var, let, const", "https://gist.githubusercontent.com/hanjoongcho/983fe388a669f1da9df13cf64f63c5f3/raw/d1587f1da1d7ead1ba695e50094dbf52daaf6e1e/var-let-const.md"),
+            Showcase("", "ES6-02", true, "Promise", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/es6/promise.md"),
+            Showcase("", "Data-Structure-01", true, "국가상수도데이터베이스표준화지침(20210101 개정)", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/design/database/standardization.md"),
+            Showcase("", "Data-Structure-02", true, "Unicode", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/data-structure/unicode.md"),
+            Showcase("", "Data-Structure-03", true, "ASCII", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/data-structure/ascii.md"),
+            Showcase("", "Database-01", true, "Oracle", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/database/oracle-01.md"),
+
+            Showcase("", "Java-01", true, "Describes annotations mainly used in Spring Framework", "https://raw.githubusercontent.com/hanjoongcho/CheatSheet/master/annotations/spring.md"),
+            Showcase("", "Java-02", true, "Java 8 - Lambda Expression", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/lambda/LambdaExpressionTest.java", true),
+            Showcase("", "Java-03", true, "Java 8 - Default Methods", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/defaultmethod/DefaultMethodTest.java", true),
+            Showcase("", "Java-04", true, "Java 8 - Functions", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/functions/FunctionFunctionalInterfaceTest.java", true),
+            Showcase("", "Java-05", true, "Java 8 - Stream Count", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/streams/StreamWithCountTest.java", true),
+            Showcase("", "Java-06", true, "Java 8 - Stream with Filter", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/streams/StreamWithFilterTest.java", true),
+            Showcase("", "Java-07", true, "Java 8 - Stream with Map", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/streams/StreamWithMapTest.java", true),
+            Showcase("", "Java-08", true, "Java 8 - Stream with Sorted", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/streams/StreamWithSortedTest.java", true),
+            Showcase("", "Java-09", true, "Java 8 - Stream with Match", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/streams/StreamWithMatchTest.java", true),
+            Showcase("", "Java-10", true, "Java 8 - Stream Reduce", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/streams/StreamReduceTest.java", true),
+            Showcase("", "Java-11", true, "Java 8 - Stream Consumer", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/consumer/ConsumerFunctionalInterfaceTest.java", true),
+            Showcase("", "Java-12", true, "Java 8 - Predicate", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/predicate/PredicateFunctionalInterfaceTest.java", true),
+            Showcase("", "Java-13", true, "Java 8 - Comparator", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/comparator/ComparatorFunctionalInterfaceTest.java", true),
+            Showcase("", "Java-14", true, "Java 8 - Suppliers", "https://raw.githubusercontent.com/hanjoongcho/java8-guides-tutorials/master/src/test/java/suppliers/SupplierFunctionalInterfaceTest.java", true),
+    ).apply { this.sortBy { item -> item.name } }
 
     private var mListItem: ArrayList<Showcase> = arrayListOf()
     private val mAdapter: ShowcaseAdapter by lazy {
