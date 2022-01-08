764a1eca3dd9cd292319c02b4977a352f7e5d1d0
DESKTOP-2T3RPD2\hanjoong.cho <hanjoongcho@gmail.com>
2022-01-08 19:02:01 +0900

    Refactoring

--- app/src/main/java/io/github/aafactory/sample/activities/WebViewSignaturePadActivity.kt
+++ app/src/main/java/io/github/aafactory/sample/activities/WebViewSignaturePadActivity.kt
@@ -1,12 +1,21 @@
 package io.github.aafactory.sample.activities
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import io.github.aafactory.commons.R
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.widget.Toast
 import io.github.aafactory.commons.activities.BaseSimpleActivity
+import io.github.aafactory.commons.extensions.makeToast
+import io.github.aafactory.sample.R
 import io.github.aafactory.sample.databinding.ActivityWebviewSignaturePadBinding
 
 class WebViewSignaturePadActivity : BaseSimpleActivity() {
     private lateinit var mBinding: ActivityWebviewSignaturePadBinding
+    private var mJSMessage: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityWebviewSignaturePadBinding.inflate(layoutInflater)
@@ -18,14 +27,41 @@ class WebViewSignaturePadActivity : BaseSimpleActivity() {
             setHomeAsUpIndicator(R.drawable.ic_cross)
         }
 
-        val webSettings = mBinding.webSignaturePad.settings
-        webSettings.setGeolocationEnabled(true)
-        webSettings.javaScriptEnabled = true
-        webSettings.allowFileAccess = true
-        webSettings.allowFileAccessFromFileURLs = true
-        webSettings.allowUniversalAccessFromFileURLs = true
+        mBinding.webSignaturePad.run {
+            settings.setGeolocationEnabled(true)
+            settings.javaScriptEnabled = true
+            settings.allowFileAccess = true
+            settings.allowFileAccessFromFileURLs = true
+            settings.allowUniversalAccessFromFileURLs = true
+            webChromeClient = WebChromeClient()
+            addJavascriptInterface(WebAppInterface(this@WebViewSignaturePadActivity), "nativeInterface")
 
-        intent.getStringExtra(TARGET_URL)?.let { mBinding.webSignaturePad.loadUrl(it) }
+            intent.getStringExtra(TARGET_URL)?.let { mBinding.webSignaturePad.loadUrl(it) }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_webview_signature_pad, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.executeJSFunction -> {
+                mBinding.webSignaturePad.loadUrl("javascript:showAlert('$mJSMessage')");
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    inner class WebAppInterface(private val mContext: Context) {
+
+        /** Show a toast from the web page  */
+        @JavascriptInterface
+        fun showToast(message: String) {
+            mJSMessage = message
+            Toast.makeText(mContext, mJSMessage, Toast.LENGTH_LONG).show()
+        }
     }
 
     companion object {
